75539c38c0cb9735b6e16ca1dbb15f4136783cc9
Rishang <rishangkumar9693@gmail.com>
2024-09-27 13:47:28 +0530

    initialized the web-app  using next js

---
+++ app/login/loginUI.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginUI = () => {
+  return (
+    <div>loginUI</div>
+  )
+}
+
+export default LoginUI
---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react' 
+import LoginUI from './loginUI'
+import Home from '../page'
+import Hero from '../LandingPage/hero'
+
+const page = () => {
+  return (
+<> 
+<p>Hello</p>
+<LoginUI/> 
+<Hero/>
+</>
+)
+}
+
+export default page
