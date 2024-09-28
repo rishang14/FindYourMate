49aee6625c3805a18e757db50bcaed038bf86d9e
Rishang <rishangkumar9693@gmail.com>
2024-09-28 13:30:04 +0530

    added framer motion with animation

--- app/LandingPage/Header.tsx
+++ app/LandingPage/Header.tsx
@@ -1,25 +1,72 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { once } from "events";
 
 const Header = () => {
   return (
     <>
-    <div className='w-full mt-[5rem] flex flex-col h-full items-center p-4'>
-    <div className='p-4 '>
-     <h1 className='text-6xl p-2 text-center  font-bold '>Let us find the perfect sports buddy to fuel your fitness and sports cravings!</h1>
-    </div>
-    <div className='p-2  w-[85%] flex justify-center '>
-     <p className='text-xl text-center font-bold text-slate-400'>Never let your sports and fitness cravings fade just because no one's free. We help you find partners and let professionals book a bowler. Always aim to upgrade yourself and stay fit!</p>
-    </div>  
-     <div className='flex justify-center p-4 items-center gap-4'>
-        <button className='p-2 rounded-md bg-gray-200 text-slate-700'>Demo 1 </button> 
-        <span className='text-5xl p-2'>|</span>
-        <button className='p-2 rounded-md  bg-gray-200 text-slate-700'>Demo 2 </button>
-     </div>
-    </div> 
+      <div className="w-full mt-[5rem] flex flex-col h-full items-center p-4">
+        <motion.div
+          className="p-4 "
+          initial={{
+            opacity: 0,
+            y: 40, 
+            x:20
+          }}
+          animate={{
+            opacity: 1,
+            y: 0, 
+            x:0
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          <motion.h1
+            className="p-2 text-center font-bold"
+            initial={{ fontSize: "40px", opacity: 0.4, lineHeight: 1 }}
+            animate={{ fontSize: "60px", lineHeight: 1, opacity: 1 }}
+          >
+            Let us find the perfect sports buddy to fuel your fitness and sports
+            cravings!
+          </motion.h1>
+        </motion.div>
+        <motion.div
+          className="p-2  w-[85%] flex justify-center"
+          initial={{
+            opacity: 0,
+            x: 60,
+          }}
+          animate={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            duration: 1,
+          }}
+        >
+          <p className="text-xl text-center font-bold text-slate-400">
+            Never let your sports and fitness cravings fade just because no
+            one's free. We help you find partners and let professionals book a
+            bowler. Always aim to upgrade yourself and stay fit!
+          </p>
+        </motion.div>
+        <div
+          className="flex justify-center p-4 items-center "
+        >
+          <button className="p-2 rounded-md bg-gray-200 text-slate-700">
+            Demo 1{" "}
+          </button>
+          <span className="text-5xl p-2">|</span>
+          <button className="p-2 rounded-md  bg-gray-200 text-slate-700">
+            Demo 2{" "}
+          </button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
